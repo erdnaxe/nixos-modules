48f9d93915151bc039100dd3bb9e70a400b19323
Alexandre Iooss <erdnaxe@crans.org>
2021-07-30 19:43:38 +0200

    Remove some filesystems support

--- base/package.nix
+++ base/package.nix
@@ -3,4 +3,7 @@
     <nixpkgs/nixos/modules/profiles/base.nix>
   ];
   environment.variables = { EDITOR = "vim"; };
+
+  # Remove ZFS, CIFS, XFS, REISERFS from supported filesystems
+  boot.supportedFilesystems = [ "btrfs" "vfat" "f2fs" "ntfs" ];
 }
