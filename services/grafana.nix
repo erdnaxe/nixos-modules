86772a37eb84ef3f355d627d57f37bafe938eee9
Alexandre Iooss <erdnaxe@crans.org>
2021-08-24 17:18:34 +0200

    Add grafana module

---
+++ services/grafana.nix
@@ -0,0 +1,66 @@
+{
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
+
+  services.grafana = {
+    enable = true;
+    port = 3001;
+    domain = "grafana.nanax.fr";
+    protocol = "http";
+    auth.anonymous.enable = true;
+    extraOptions = {
+      ALERTING_ENABLED = "false";
+      METRICS_ENABLED = "false";
+      ANALYTICS_REPORTING_ENABLED = "false";
+      SNAPSHOTS_ENABLED = "false";
+      PANELS_DISABLE_SANITIZE_HTML = "true";
+    };
+  };
+
+  services.nginx = {
+    enable = true;
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+    virtualHosts."grafana.nanax.fr" = {
+      enableACME = true;
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:3001";
+      };
+    };
+  };
+
+  # https://github.com/NixOS/nixpkgs/pull/134174
+  systemd.services.grafana = {
+    serviceConfig = {
+      # Hardening
+      CapabilityBoundingSet = [ "" ];
+      DeviceAllow = [ "" ];
+      LockPersonality = true;
+      MemoryDenyWriteExecute = true;
+      NoNewPrivileges = true;
+      PrivateDevices = true;
+      PrivateTmp = true;
+      PrivateUsers = true;
+      ProcSubset = "pid";
+      ProtectClock = true;
+      ProtectControlGroups = true;
+      ProtectHome = true;
+      ProtectHostname = true;
+      ProtectKernelLogs = true;
+      ProtectKernelModules = true;
+      ProtectKernelTunables = true;
+      ProtectProc = "invisible";
+      ProtectSystem = "full";
+      RemoveIPC = true;
+      RestrictAddressFamilies = [ "AF_INET" "AF_INET6" ];
+      RestrictNamespaces = true;
+      RestrictRealtime = true;
+      RestrictSUIDSGID = true;
+      SystemCallArchitectures = "native";
+      SystemCallFilter = [ "@system-service" "~@privileged" "~@resources" ];
+      UMask = "0077";
+    };
+  };
+}
