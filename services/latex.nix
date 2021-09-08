396932fb55eb354a45ad45fc2c36ca43b8c4cca8
Alexandre Iooss <erdnaxe@crans.org>
2021-09-08 09:56:33 +0200

    Add latex service

---
+++ services/latex.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    (texlive.combine {
+      inherit (texlive) scheme-medium
+
+      # beamerposter
+      beamerposter type1cm tcolorbox environ csquotes biber
+
+      titlepic moderncv fontawesome biblatex;
+    })
+  ];
+}
