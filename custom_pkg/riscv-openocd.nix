b425e8ae01ee44be87a84cdae9bacc16b5c10254
Alexandre Iooss <erdnaxe@crans.org>
2022-01-10 12:36:22 +0100

    Add riscv-openocd package

---
+++ custom_pkg/riscv-openocd.nix
@@ -0,0 +1,39 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation rec {
+  pname = "riscv-openocd";
+  version = "2019-12-06-unstable";
+
+  src = fetchFromGitHub {
+    owner = "riscv";
+    repo = "riscv-openocd";
+    rev = "aec5cca15b41d778fb85e95b38a9a552438fec6a";
+    sha256 = "1msz895w3xwv9r20hi3qhpb5xcprfyjx0y24gv9icm003y5dchjm";
+    fetchSubmodules = true;
+  };
+
+  nativeBuildInputs = [ pkg-config autoreconfHook ];
+  buildInputs = [ hidapi libftdi1 libusb1 jimtcl ];
+
+  configureFlags = [
+    "--enable-ftdi"
+    "--disable-internal-jimtcl"
+  ];
+
+  NIX_CFLAGS_COMPILE = [
+    "-Wno-error"
+    #"-Wno-error=cpp"
+    #"-Wno-error=strict-prototypes" # fixes build failure with hidapi 0.10.0
+  ];
+
+  postInstall = lib.optionalString stdenv.isLinux ''
+    mkdir -p "$out/etc/udev/rules.d"
+    rules="$out/share/openocd/contrib/60-openocd.rules"
+    if [ ! -f "$rules" ]; then
+        echo "$rules is missing, must update the Nix file."
+        exit 1
+    fi
+    ln -s "$rules" "$out/etc/udev/rules.d/"
+  '';
+}
+
