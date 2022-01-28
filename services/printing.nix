8d591a06b87ced4ae72a377120339f8d84f4b079
Alexandre Iooss <erdnaxe@crans.org>
2022-01-28 11:18:30 +0100

    Add cups hardening

--- services/printing.nix
+++ services/printing.nix
@@ -9,6 +9,24 @@
   };
   services.system-config-printer.enable = true;
 
+  systemd.services.cups.serviceConfig = {
+    # Hardening
+    CapabilityBoundingSet = [ "CAP_CHOWN" "CAP_AUDIT_WRITE" "CAP_DAC_OVERRIDE" "CAP_FSETID" "CAP_KILL" "CAP_NET_BIND_SERVICE" "CAP_SETGID" "CAP_SETUID" ];
+    PrivateTmp = true;
+    ProtectClock = true;
+    ProtectControlGroups = true;
+    ProtectHome = true;
+    ProtectHostname = true;
+    ProtectKernelLogs = true;
+    ProtectKernelModules = true;
+    ProtectKernelTunables = true;
+    RestrictAddressFamilies = [ "AF_INET" "AF_INET6" "AF_NETLINK" "AF_UNIX" ];
+    RestrictNamespaces = true;
+    RestrictRealtime = true;
+    RestrictSUIDSGID = true;
+    SystemCallArchitectures = "native";
+  };
+
   # Scanner
   # Disabled because of incompatibility with cups
   #hardware.sane.enable = true;
