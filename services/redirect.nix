9dbe816246336f1a673b5c186ea225f687ccfa05
Alexandre Iooss <erdnaxe@crans.org>
2022-02-05 17:44:43 +0100

    Remove ens-ps.fr redirection

--- services/redirect.nix
+++ services/redirect.nix
@@ -2,12 +2,6 @@
   networking.firewall.allowedTCPPorts = [ 80 443 ];
 
   services.nginx.virtualHosts = {
-    "ens-ps.fr" = {
-      enableACME = true;
-      forceSSL = true;
-      serverAliases = [ "www.ens-ps.fr" ];
-      locations."/".return = "302 https://services.crans.org$request_uri";
-    };
     "nanax.fr" = {
       enableACME = true;
       forceSSL = true;
